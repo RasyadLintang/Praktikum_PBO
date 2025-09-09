cee7358739320eda3e8ede9ea58e3e6e6fa0c976
RasyadLintang <rasyadlintang15@gmail.com>
2025-09-09 14:29:59 +0700

    Add files via upload

---
+++ Rasyad_proyek1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rasyad_proyek1;
+
+/**
+ *
+ * @author Pavilion Gaming
+ */
+public class Rasyad_proyek1 {
+public static void main(String[] args) {
+        System.out.println("Nama    : Rasyad Lintang Wahyunindar");
+        System.out.println("NPM     : 2420506038");
+        System.out.println("Alamat  : Kota Magelang");
+        System.out.println("No_HP   : 089688394984");
+    }
+}
